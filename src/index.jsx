cbb354fa7afe1c7c991d8cda2e7effd82f2aed8d
Rohan Mazumdar <rohan1443@gmail.com>
2016-12-13 19:33:59 +0530

    commiting all the concerned files into the repo

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import App from './jsx/components/app.jsx';
+
+render( <AppContainer><App/></AppContainer>, document.querySelector("#app"));
+
+if (module && module.hot) {
+  module.hot.accept('./jsx/components/app.jsx', () => {
+    const App = require('./jsx/components/app.jsx').default;
+    render(
+      <AppContainer>
+        <App/>
+      </AppContainer>,
+      document.querySelector("#app")
+    );
+  });
+}
